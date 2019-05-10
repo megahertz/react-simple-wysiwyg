45f6a7bea8407eafef04338d43fe7eccc3dfc40d
Alexey Prokhorov <megahertz@l96.ru>
2019-05-10 22:59:31 +0400

    Minimal readme

--- examples/typescript/src/App.tsx
+++ examples/typescript/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import DefaultEditor from 'react-simple-wysiwyg/DefaultEditor';
+import { DefaultEditor } from 'react-simple-wysiwyg';
 
 export default function App() {
   const [html, setHtml] = React.useState('first line <br> <b>second</b> line');
