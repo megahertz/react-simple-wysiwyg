949a4311f91ca90b0ffa9e8ed3eadca64573f8ce
Alexey Prokhorov <megahertz@l96.ru>
2019-05-11 02:11:49 +0400

    Add Editor component to exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as ContentEditable } from './ContentEditable';
 export { default as DefaultEditor } from './DefaultEditor';
+export { default as Editor } from './Editor';
 export * from './toolbar';
